16a1dc6195abdd09b4f95ec7d3b080a12757a06c
Yaseen <yasin.inflack@bkash.com>
2023-01-11 12:13:16 +0600

    FM

---
+++ src/main/java/com/bkash/dummysavings/DummySavingsApplication.java
@@ -0,0 +1,13 @@
+package com.bkash.dummysavings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummySavingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummySavingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bkash/dummysavings/DummySavingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkash.dummysavings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummySavingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
